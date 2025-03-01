3fd84e0f0e9a2ff5c3ec1fd410a76f18d321de39
Md. Ibrahim <111380458+Tetrapico@users.noreply.github.com>
2025-03-01 20:01:47 +0600

    CRT_cf1770.cpp

---
+++ CRT_cf1770.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+#define MULTI int _T; cin >> _T; while(_T--)
+using namespace std;
+typedef long long ll;
+ 
+const int N = 105;
+const int INF = 0x3f3f3f3f;
+template <typename T> bool chkmin (T &x, T y) {return y < x ? x = y, 1 : 0;}
+template <typename T> bool chkmax (T &x, T y) {return y > x ? x = y, 1 : 0;}
+ 
+int n;
+ll a[N];
+ 
+int cnt[N];
+ 
+int main () {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	MULTI {
+		cin >> n;
+		for (int i = 1;i <= n;++i) {
+			cin >> a[i];
+		}
+		
+		int isDistinct = 1;
+		sort(a + 1, a + n + 1);
+		for (int i = 1;i <= n - 1;++i) {
+			if (a[i] == a[i + 1]) isDistinct = 0;
+		}
+		if (isDistinct == 0) {
+			cout << "NO" << endl;
+			continue;
+		}
+		
+		int CRT_able = 1;
+		for (int mod = 2;mod <= n / 2;++mod) {
+			fill(cnt, cnt + mod, 0);
+			for (int i = 1;i <= n;++i) {
+				cnt[a[i] % mod]++;
+			}
+			if (*min_element(cnt, cnt + mod) >= 2) CRT_able = 0;
+		}
+		cout << (CRT_able ? "YES" : "NO") << endl;
+	}
+}
